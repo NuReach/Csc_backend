eacb2577b2e908d044a1a9de3d5c99790d92329a
NuReach <nureach46@gmail.com>
2024-06-25 23:19:00 +0700

    cmt api

---
+++ app/Http/Controllers/Api/CommentController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Models\Comment;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CommentController extends Controller
+{
+
+    public function index () {
+        $comments = Comment::limit(8)
+        ->paginate();
+        return response()->json($comments, 200);
+    }
+
+    public function store(StoreCommentRequest $request, $videoId)
+    {
+        $comment = new Comment([
+            'content' => $request->content,
+            'video_id' => $videoId,
+            'user_id' => Auth::id(),
+        ]);
+
+        $comment->save();
+
+        return Response::json($comment, 201); // Created status code
+    }
+
+    public function update(UpdateCommentRequest $request, $videoId, $commentId)
+    {
+        $comment = Comment::findOrFail($commentId);
+
+        // Check authorization (ensure user can only edit their own comments)
+        if ($comment->user_id !== Auth::id()) {
+            return Response::json(['error' => 'Unauthorized'], 403); // Forbidden status code
+        }
+
+        $comment->update($request->only('content'));
+
+        return Response::json($comment, 200); // OK status code
+    }
+    public function destroy($commentId)
+    {
+        $comment = Comment::findOrFail($commentId);
+
+        // Check authorization (ensure user can only delete their own comments)
+        if ($comment->user_id !== Auth::id()) {
+            return Response::json(['error' => 'Unauthorized'], 403); // Forbidden status code
+        }
+
+        $comment->delete();
+
+        return Response::json(null, 204); // No Content status code
+    }
+}
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\Video;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Comment extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+    public function video()
+    {
+        return $this->belongsTo(Video::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
