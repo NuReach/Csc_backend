cece666104d2879f63a0e954fc7c50a53b97b9d3
NuReach <nureach46@gmail.com>
2024-07-05 17:44:55 +0700

    create resource

--- app/Http/Controllers/Api/VideoResoucreController.php
+++ app/Http/Controllers/Api/VideoResoucreController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Models\Resource;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class VideoResoucreController extends Controller
 {
